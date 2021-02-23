6620ab898f061771e25e1c3810f3a633af7dee87
thewalkingduff <62387646+thewalkingduff@users.noreply.github.com>
2021-02-22 21:02:44 -0500

    removed some redundancy in app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-Good Foody
+// Good Foody
 
 const goodFoods = document.querySelectorAll('.good')
 
@@ -24,32 +24,3 @@ function badSound() {
 for(let badFood of badFoods){
         badFood.addEventListener('click', badSound, false)
     }
-
-    
-
-
-    function goodSound() {
-        const sound = document.getElementById("goodFoody")
-        sound.play();
-    }
-
-    const goodFoods = document.querySelectorAll('.good')
-    
-    for(let goodFood of goodFoods){
-        goodFood.addEventListener('click', goodSound, false)
-    }
-    
-    // Bad Foody
-    
-    
-    
-    function badSound() {
-        const sound = document.getElementById("badFoody");
-            sound.play();
-        }
-
-    const badFoods = document.querySelectorAll('.bad')
-       
-    for(let badFood of badFoods){
-            badFood.addEventListener('click', badSound, false)
-        }
