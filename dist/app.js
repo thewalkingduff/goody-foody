6c20fcf93a6c92611f9375663e7e1a61222c0095
Brendan Duffy <bduffy@devduffy.com>
2021-09-03 10:27:12 -0400

    delete duplicate tsconfig

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,15 @@
+"use strict";
 // Good Foody
-const goodFoods = [...document.querySelectorAll('.good')];
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+var goodFoods = __spreadArray([], document.querySelectorAll('.good'), true);
 console.log(goodFoods);
 var foody;
 (function (foody) {
@@ -7,19 +17,21 @@ var foody;
     foody["bad"] = "badFoody";
 })(foody || (foody = {}));
 function goodSound() {
-    let sound = document.getElementById(foody.good);
+    var sound = document.getElementById(foody.good);
     return sound.play();
 }
-for (let goodFood of goodFoods) {
+for (var _i = 0, goodFoods_1 = goodFoods; _i < goodFoods_1.length; _i++) {
+    var goodFood = goodFoods_1[_i];
     goodFood.addEventListener('click', goodSound, false);
 }
 // Bad Foody
-const badFoods = [...document.querySelectorAll('.bad')];
+var badFoods = __spreadArray([], document.querySelectorAll('.bad'), true);
 console.log(badFoods);
 function badSound() {
-    let sound = document.getElementById(foody.bad);
+    var sound = document.getElementById(foody.bad);
     return sound.play();
 }
-for (let badFood of badFoods) {
+for (var _a = 0, badFoods_1 = badFoods; _a < badFoods_1.length; _a++) {
+    var badFood = badFoods_1[_a];
     badFood.addEventListener('click', badSound, false);
 }
